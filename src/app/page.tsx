74ed6743014c92741878e9aa289e3e0cac26a68b
RanaIsHere <60571003+RanaIsHere@users.noreply.github.com>
2025-08-21 21:52:14 +0700

    Home page search button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import Image from "next/image";
 import Header from "../../components/header";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Header />
+      <Header />
 
-        {/* <p>Hello world!</p> */}
+      <main className={styles.main}>
+        <p>Welcome to Sucrose</p>
+        <p>Search for meals, drinks & dessert</p>
+
+        <div className={styles.searchGroup}>
+          <div className={styles.searchBarContainer}>
+            <Image className={styles.searchBarIcon} src="icons/search-icon.svg" alt="Search" width={48} height={48} />
+            <input type="text" name="search" id="searchbar" className={styles.searchBar} placeholder="Search" />
+          </div>
+          <a href="#sort">
+            <Image src="icons/sort-btn.svg" alt="Sort list button" width={48} height={48} />
+          </a>
+        </div>
       </main>
     </div>
   );
