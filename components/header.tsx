f3d78e106d83441b3623fcea345ed057ef72e06f
RanaIsHere <60571003+RanaIsHere@users.noreply.github.com>
2025-08-21 20:49:50 +0700

    Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,20 @@
+import styles from './header.module.css'
+import Image from 'next/image'
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <Image src="/icons/sucrose-logo.svg" alt='Sucrose Logo' width={192} height={64} />
+
+            <div className={styles.buttonGroup}>
+                <a href='#cart'>
+                    <Image src="/icons/cart-btn.svg" alt="View cart" width={48} height={48} />
+                </a>
+
+                <a href='#menu'>
+                    <Image src="/icons/menu-btn.svg" alt="Menu button" width={48} height={48} />
+                </a>
+            </div>
+        </header>
+    )
+}
---
+++ src/app/cart/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Cart() {
+    return (
+        <div>
+            <p>Cart</p>
+        </div>
+    );
+}
