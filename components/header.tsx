0fa61e096732865a6665f329790e2bed8a5f9a09
RanaIsHere <60571003+RanaIsHere@users.noreply.github.com>
2025-08-22 13:44:10 +0700

    Added sidebar

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,21 @@
+'use client'
+
+import { useState } from 'react';
 import styles from './header.module.css'
 import Image from 'next/image'
+import Sidebar from './sidebar';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className={styles.header}>
+            <Sidebar isOpenable={isOpen} toggleSidebar={openSidebar} />
+
             <Image src="/icons/sucrose-logo.svg" alt='Sucrose Logo' width={192} height={64} />
 
             <div className={styles.buttonGroup}>
@@ -11,7 +23,7 @@ export default function Header() {
                     <Image src="/icons/cart-btn.svg" alt="View cart" width={48} height={48} />
                 </a>
 
-                <a href='#menu'>
+                <a onClick={openSidebar}>
                     <Image src="/icons/menu-btn.svg" alt="Menu button" width={48} height={48} />
                 </a>
             </div>
---
+++ components/sidebar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import styles from './sidebar.module.css'
+
+import Link from 'next/link'
+
+type OpenableSidebarProps = {
+    isOpenable: boolean,
+    toggleSidebar: () => void,
+}
+
+export default function Sidebar({ isOpenable, toggleSidebar }: OpenableSidebarProps) {
+    const login: boolean = false;
+
+    return (
+        <aside className={`${styles.sidebar} ${isOpenable ? styles.sidebarOpen : ''} `} onClick={toggleSidebar}>
+            <div className={styles.sidebarTitle}>
+                <Image src="icons/x-mark-icon.svg" alt="Close" width={64} height={64} onClick={toggleSidebar} />
+                <p>Menu</p>
+            </div>
+
+            <div className={styles.sidebarGroup}>
+                <Link href="/">Home</Link>
+                <Link href="/cart">Cart</Link>
+                <Link href="/">Orders</Link>
+                <Link href="/">Account</Link>
+                {login ? <Link href="/">Sign Out</Link> : <Link href="/">Login</Link>}
+            </div>
+        </aside>
+    )
+}
